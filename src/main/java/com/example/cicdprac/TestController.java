b10a8835d8ed4cbd6b3eb7c77336ca180555f01b
roy656 <v0o0605@gmail.com>
2022-09-05 20:14:23 +0900

    post api 생성

--- src/main/java/com/example/cicdprac/TestController.java
+++ src/main/java/com/example/cicdprac/TestController.java
@@ -1,6 +1,7 @@
 package com.example.cicdprac;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,10 @@ public class TestController {
     @GetMapping("/api/check")
     public String checkTest() {
         return "OK!!!";
+    }
+
+    @PostMapping("/api/postcheck")
+    public void post() {
+
     }
 }
