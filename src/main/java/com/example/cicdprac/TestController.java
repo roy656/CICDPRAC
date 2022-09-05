9a6c1f98e44540ae466483a7d8affbf2474212c3
roy656 <v0o0605@gmail.com>
2022-09-05 18:46:04 +0900

    .

---
+++ src/main/java/com/example/cicdprac/TestController.java
@@ -0,0 +1,13 @@
+package com.example.cicdprac;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/api/check")
+    public String checkTest() {
+        return "OK!!";
+    }
+}
